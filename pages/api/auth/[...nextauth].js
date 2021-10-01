3c19e61ba0ec9215535f88728ed25f636defd93e
Vladislav <selies@mail.ru>
2021-10-01 02:43:24 +0100

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,13 +1,14 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import NextAuth from 'next-auth'
+import Providers from 'next-auth/providers'
 
-export default NextAuth({
-  // Configure one or more authentication providers
+const options = {
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+    Providers.Google({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET
     }),
-    // ...add more providers here
   ],
-})
+  debug: false
+}
+
+export default (req, res) => NextAuth(req, res, options)
