1c7f7b627efbde3a1669d9c46487ecff6c719b7c
Vladislav <selies@mail.ru>
2021-10-15 19:50:58 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Lato', ...fontFamily.sans],
+        sans: ['Source Sans Pro', ...fontFamily.sans],
         serif: ['EB Garamond', ...fontFamily.serif]
       },
     },
