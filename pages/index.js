f9b6c983190420025a7fcb71b4ba7e1efbc7b7f3
Vladislav <selies@mail.ru>
2021-10-15 02:52:25 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,7 @@ export default function Home() {
       <div className="bg-white">
         <nav className="flex flex-col justify-start md:flex-row md:justify-between w-full max-w-7xl p-8 mx-auto my-0 text-gray-900 text-[18px]">
           <div>
-            <a href="/" id="logo" className="text-gray-900 font-semibold">Chernivtsi National Theatre </a>
+            <a href="/" id="logo" className="text-gray-900 font-sans font-bold">Chernivtsi National Theatre </a>
           </div>
           <div className='mt-5'>
             <button type='button' className="mobile-burger-menu block text-gray-900 md:hidden" onClick={handleClick}>
@@ -71,9 +71,9 @@ export default function Home() {
         </div>
         <div className="w-[20rem] h-[22rem] ml-16 mt-12 md:w-[32rem] md:h-[20rem] bg-white absolute z-10	md:mt-40 md:ml-64 ">
           <h3 className="font-serif font-semibold text-5xl py-5 px-7">The Nutcracker</h3>
-          <h4 className="text-base text-gray-400 mb-2 px-7 italic">1 March–28 May 2022</h4>
-          <h4 className="text-base px-7">The performance lasts about 3 hours, including two intervals.</h4>
-          <h4 className="text-base px-7 mt-1">Tickets price: £40-£120</h4>
+          <h4 className="text-base font-sans text-gray-400 mb-2 px-7 italic">1 March–28 May 2022</h4>
+          <h4 className="text-base font-sans px-7">The performance lasts about 3 hours, including two intervals.</h4>
+          <h4 className="text-base font-sans px-7 mt-1">Tickets price: £40-£120</h4>
           <button type='button' className="text-gray-900  px-10 py-2 border-2 border-gray-600 text-gray-900 hover:bg-gray-900 hover:text-gray-100 duration-700 mt-7 px-7 ml-7">Buy tickets</button>
         </div>
       </div>
