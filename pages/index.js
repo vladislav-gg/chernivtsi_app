91c2f5f4432f1a2c47518fb0ac95b1d6a08467e6
Vladislav <selies@mail.ru>
2021-10-18 00:00:33 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -20,6 +20,10 @@ export default function Home() {
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
         <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,400;0,600;0,700;0,900;1,400;1,600&display=swap" rel="stylesheet"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"/>
+
       </Head>
       <Navigation/>
       <Hero/>
