fe49d4c6250ad6ac4b09ae9bd61de82f4377c57a
Vladislav <selies@mail.ru>
2021-10-18 00:30:15 +0100

    Update events.js

--- pages/events.js
+++ pages/events.js
@@ -107,6 +107,102 @@ export default function events() {
 					</div>
 				</div>
 			</div>
+			<div className="max-w-7xl mx-auto">
+				<div>
+					<div className="events-section w-full h-full">
+						<div className="flex flex-col md:flex-row md:justify-between mt-10  max-w-7xl mx-auto border-b border-gray-600 border-t border-gray-600 py-8">
+							<div className="event-date text-md text-gray-900 flex flex-row justify-start items-center">
+								<span className="ml-10 font-serif font-bold">Th</span>
+								<span className="ml-10 font-serif font-bold">23.09</span>
+								<span className="ml-10 font-serif font-bold">19:00</span>
+							</div>
+							<div className="flex flex-col justify-center ml-10 mt-3 md:mt-0 md:ml-0">
+								<h2 className="text-4xl text-gray-900 font-serif font-medium">
+									Swan Lake
+								</h2>
+								<h4 className="text-lg text-gray-500 font-sans italic">
+									Commedia per musica in four acts by Wolfgang Amadeus Mozart
+								</h4>
+							</div>
+							<div className="flex md:items-center ml-10 mt-5 md:ml-0 md:mt-0">
+								<button
+									type="button"
+									className="text-gray-900  px-10 py-2 border-2 border-gray-600 text-gray-900 hover:bg-gray-900 hover:text-gray-100 duration-700"
+								>
+									Buy tickets
+								</button>
+							</div>
+						</div>
+						<div className="flex flex-col md:flex-row md:justify-between mt-10  max-w-7xl mx-auto border-b md:border-b-0 border-gray-600 border-t border-gray-600 py-8">
+							<div className="event-date text-md text-gray-900 flex flex-row justify-start items-center">
+								<span className="ml-10 font-serif font-bold">Fr</span>
+								<span className="ml-10 font-serif font-bold">24.09</span>
+								<span className="ml-10 font-serif font-bold">17:00</span>
+							</div>
+							<div className="flex flex-col justify-center ml-10 mt-3 md:mt-0 md:ml-0">
+								<h2 className="text-4xl text-gray-900 font-serif font-medium">
+									The Nutcracker
+								</h2>
+								<h4 className="text-lg text-gray-500 font-sans italic">
+									Commedia per musica in four acts by Wolfgang Amadeus Mozart
+								</h4>
+							</div>
+							<div className="flex md:items-center ml-10 mt-5 md:ml-0 md:mt-0">
+								<button className="text-gray-900  px-10 py-2 border-2 border-gray-600 text-gray-900 hover:bg-gray-900 hover:text-gray-100 duration-700">
+									Buy tickets
+								</button>
+							</div>
+						</div>
+						<div className="flex flex-col md:flex-row md:justify-between mt-10  max-w-7xl mx-auto border-b border-gray-600 border-t md:border-t-0 border-gray-600 py-8">
+							<div className="event-date text-md text-gray-900 flex flex-row justify-start items-center">
+								<span className="ml-10 font-serif font-bold">Fr</span>
+								<span className="ml-10 font-serif font-bold">24.09</span>
+								<span className="ml-10 font-serif font-bold">20:00</span>
+							</div>
+							<div className="flex flex-col justify-center ml-10 mt-3 md:mt-0 md:ml-0">
+								<h2 className="text-4xl text-gray-900 font-serif font-medium">
+									Romeo and Juliet
+								</h2>
+								<h4 className="text-lg text-gray-500 font-sans italic">
+									Commedia per musica in four acts by Wolfgang Amadeus Mozart
+								</h4>
+							</div>
+							<div className="flex md:items-center ml-10 mt-5 md:ml-0 md:mt-0">
+								<button
+									type="button"
+									className="text-gray-900  px-10 py-2 border-2 border-gray-600 text-gray-900 hover:bg-gray-900 hover:text-gray-100 duration-700"
+								>
+									Buy tickets
+								</button>
+							</div>
+						</div>
+						<div className="flex flex-col md:flex-row md:justify-between mt-10  max-w-7xl mx-auto border-b border-gray-600 border-t border-gray-600 py-8">
+							<div className="event-date text-md text-gray-900 flex flex-row justify-start items-center">
+								<span className="ml-10 font-serif font-bold">Sat</span>
+								<span className="ml-10 font-serif font-bold">25.09</span>
+								<span className="ml-10 font-serif font-bold">20:00</span>
+							</div>
+							<div className="flex flex-col justify-center ml-10 mt-3 md:mt-0 md:ml-0">
+								<h2 className="text-4xl text-gray-900 font-serif font-medium">
+									The Magic Flute
+								</h2>
+								<h4 className="text-lg text-gray-500 font-sans italic max-w-[600px]">
+									David McVicar’s spectacular production of Mozart’s comic
+									opera.
+								</h4>
+							</div>
+							<div className="flex md:items-center ml-10 mt-5 md:ml-0 md:mt-0">
+								<button
+									type="button"
+									className="text-gray-900  px-10 py-2 border-2 border-gray-600 text-gray-900 hover:bg-gray-900 hover:text-gray-100 duration-700"
+								>
+									Buy tickets
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 			<Footer/>
 		</div>
 	);
