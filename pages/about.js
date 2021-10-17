e6fd5547ee2b2f1f71dbdab3b8627b8815ca7bf1
Vladislav <selies@mail.ru>
2021-10-18 00:30:13 +0100

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -9,15 +9,15 @@ export default function about() {
                 <Navigation/>
             </div>
             <div className='text-gray-800 font-serif text-4xl max-w-6xl mx-auto mt-16 leading-[3rem]'>
-                <h1 className='py-7'>I decided to create this website for my hometown theatre as I thought it would be a nice idea. This website is probably half finished at this moment and I already hate how it looks. But, regardless of the outcome I learn a lot on the day to day basis. 
+                <h1 className='py-7'>I decided to create this website for my <a href='https://dramtheater.cv.ua/' className='underline'>hometown theatre</a> as I thought it would be a nice idea. This website is probably half finished at this moment and I already hate how it looks. But, regardless of the outcome I learn a lot on the day to day basis. 
                 </h1>
 
                 <h1 class='pb-7'>
-                Hopefully, I will be able to implement sign-in form with tickets purchase in the near future. 
+                Hopefully, I will be able to implement a sign-in form with tickets purchase in the near future. 
                 </h1>
 
                 <h1 className=''>
-                Tune in. IDK why I write this all right now, but I don't want my pages to be empty, so I will include some content in each. 
+                IDK why I write this all right now, but I don't want my pages to be empty, so I will have some content on each page. 
                 </h1>
                 
                 <h1 className='pt-7'>
