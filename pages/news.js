0b0720811e16845c663f3d0155f6c53f25366225
Vladislav <selies@mail.ru>
2021-10-19 02:29:07 +0100

    Update news.js

--- pages/news.js
+++ pages/news.js
@@ -8,92 +8,108 @@ export default function News() {
             <div className="bg-white">
                 <Navigation/>
             </div>
-            <div className='max-w-[85%] mx-auto mt-16 h-full relative'>
-                <h1 className='font-serif text-6xl '>News</h1>
-                <div className='flex flex-col md:flex-row w-full h-full mt-12'>
-                    <div className='md:max-w-md'>
-                        <div className='left-col'>
-                            <img src='/images/the_nutckracker.jpg'></img>
-                            <div className="flex flex-col pt-5"> 
-                                <p className="text-xl text-gray-500 mt-1">20 September 2021</p>
-                                <h3 className="text-2xl text-gray-900 mt-5">New ballet in our theatre</h3>
-                                <a href='/'>
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 flex-no-shrink fill-current mt-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1} d="M17 8l4 4m0 0l-4 4m4-4H3" />
-                                </svg>
-                                </a>   
+            <div>
+            <section className='max-w-[85%] mx-auto mt-16 h-full relative'>
+                <h1 className='font-serif text-7xl font-medium '>News</h1>
+                    <div className='flex flex-col md:flex-row w-full h-full mt-12'>
+                        <div className='md:max-w-md'>
+                            <div className='left-col hover:shadow-lg transition duration-200 ease-in p-5'>
+                                <a href='/shows/nutckracker'>
+                                <img src='/images/the_nutckracker.jpg'></img>
+                                <div className="flex flex-col pt-5"> 
+                                    <p className="text-xl text-gray-500 mt-1">20 September 2021</p>
+                                    <h3 className="text-2xl text-gray-900 mt-5">New ballet in our theatre</h3>
+                                    <a href='/shows/nutckracker'>
+                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 flex-no-shrink fill-current mt-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1} d="M17 8l4 4m0 0l-4 4m4-4H3" />
+                                        </svg>
+                                    </a>   
+                                </div>
+                                </a>
+                            </div>
+                            <div className='left-col mt-1 left-col hover:shadow-lg transition duration-200 ease-in p-5'>
+                                <a href='/shows/nutckracker'>
+                                <img src='/images/spyashaya_krasavica.jpg'></img>
+                                <div className="flex flex-col pt-5"> 
+                                    <p className="text-xl text-gray-500 mt-1">20 September 2021</p>
+                                    <h3 className="text-2xl text-gray-900 mt-5">New ballet in our theatre</h3>
+                                    <a href='/shows/nutckracker'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 flex-no-shrink fill-current mt-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1} d="M17 8l4 4m0 0l-4 4m4-4H3" />
+                                    </svg>
+                                    </a>   
+                                </div>
+                                </a>  
                             </div>
                         </div>
-                        <div className='left-col mt-16'>
-                            <img src='/images/the_nutckracker.jpg'></img>
-                            <div className="flex flex-col pt-5"> 
-                                <p className="text-xl text-gray-500 mt-1">20 September 2021</p>
-                                <h3 className="text-2xl text-gray-900 mt-5">New ballet in our theatre</h3>
-                                <a href='/'>
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 flex-no-shrink fill-current mt-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1} d="M17 8l4 4m0 0l-4 4m4-4H3" />
-                                </svg>
-                                </a>   
+                        <div className='right-col md:ml-10 h-full w-full mt-16 md:mt-0 hover:shadow-lg transition duration-200 ease-in p-5'>
+                            <a href='/shows/nutckracker'>
+                                <img src='/images/my_pic.jpg'></img>
+                                <div className="flex flex-col pt-5"> 
+                                    <p className="text-xl text-gray-500 mt-1">20 September 2021</p>
+                                    <h3 className="text-2xl text-gray-900 mt-5">New ballet in our theatre</h3>
+                                    <a href='/'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 flex-no-shrink fill-current mt-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1} d="M17 8l4 4m0 0l-4 4m4-4H3" />
+                                    </svg>
+                                    </a>   
+                                </div>
+                            </a>
+                            </div>
+                    </div>
+                    <div className='flex flex-col md:flex-row-reverse w-full h-full mt-12'>
+                        <div className='md:max-w-md'>
+                            <div className='left-col hover:shadow-lg transition duration-200 ease-in p-5'>
+                                <a href='/shows/nutckracker'>
+                                <img src='/images/the_nutckracker.jpg'></img>
+                                <div className="flex flex-col pt-5"> 
+                                    <p className="text-xl text-gray-500 mt-1">20 September 2021</p>
+                                    <h3 className="text-2xl text-gray-900 mt-5">New ballet in our theatre</h3>
+                                    <a href='/shows/nutckracker'>
+                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 flex-no-shrink fill-current mt-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1} d="M17 8l4 4m0 0l-4 4m4-4H3" />
+                                        </svg>
+                                    </a>   
+                                </div>
+                                </a>
+                            </div>
+                            <div className='left-col mt-1 left-col hover:shadow-lg transition duration-200 ease-in p-5'>
+                                <a href='/shows/nutckracker'>
+                                <img src='/images/spyashaya_krasavica.jpg'></img>
+                                <div className="flex flex-col pt-5"> 
+                                    <p className="text-xl text-gray-500 mt-1">20 September 2021</p>
+                                    <h3 className="text-2xl text-gray-900 mt-5">New ballet in our theatre</h3>
+                                    <a href='/shows/nutckracker'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 flex-no-shrink fill-current mt-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1} d="M17 8l4 4m0 0l-4 4m4-4H3" />
+                                    </svg>
+                                    </a>   
+                                </div>
+                                </a>  
                             </div>
                         </div>
-                    </div>
-                    <div className='right-col md:ml-10 h-full w-full mt-16 md:mt-0'>
-                        <img src='/images/my_pic.jpg'></img>
-                        <div className="flex flex-col pt-5"> 
-                            <p className="text-xl text-gray-500 mt-1">20 September 2021</p>
-                            <h3 className="text-2xl text-gray-900 mt-5">New ballet in our theatre</h3>
-                            <a href='/'>
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 flex-no-shrink fill-current mt-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1} d="M17 8l4 4m0 0l-4 4m4-4H3" />
-                            </svg>
-                            </a>   
-                        </div>
-                    </div>
-                </div>
-                <div className='flex flex-col md:flex-row-reverse w-full h-full mt-12'>
-                    <div className='md:max-w-md'>
-                        <div className='left-col'>
-                            <img src='/images/the_nutckracker.jpg'></img>
-                            <div className="flex flex-col pt-5"> 
-                                <p className="text-xl text-gray-500 mt-1">20 September 2021</p>
-                                <h3 className="text-2xl text-gray-900 mt-5">New ballet in our theatre</h3>
-                                <a href='/'>
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 flex-no-shrink fill-current mt-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1} d="M17 8l4 4m0 0l-4 4m4-4H3" />
-                                </svg>
-                                </a>   
+                        <div className='right-col md:mr-10 h-full w-full mt-16 md:mt-0 hover:shadow-lg transition duration-200 ease-in p-5'>
+                            <a href='/shows/nutckracker'>
+                                <img src='/images/my_pic.jpg'></img>
+                                <div className="flex flex-col pt-5"> 
+                                    <p className="text-xl text-gray-500 mt-1">20 September 2021</p>
+                                    <h3 className="text-2xl text-gray-900 mt-5">New ballet in our theatre</h3>
+                                    <a href='/'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 flex-no-shrink fill-current mt-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1} d="M17 8l4 4m0 0l-4 4m4-4H3" />
+                                    </svg>
+                                    </a>   
+                                </div>
+                            </a>
                             </div>
-                        </div>
-                        <div className='left-col mt-16'>
-                            <img src='/images/the_nutckracker.jpg'></img>
-                            <div className="flex flex-col pt-5"> 
-                                <p className="text-xl text-gray-500 mt-1">20 September 2021</p>
-                                <h3 className="text-2xl text-gray-900 mt-5">New ballet in our theatre</h3>
-                                <a href='/'>
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 flex-no-shrink fill-current mt-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1} d="M17 8l4 4m0 0l-4 4m4-4H3" />
-                                </svg>
-                                </a>   
-                            </div>
-                        </div>
                     </div>
-                    <div className='right-col md:mr-10 h-full w-full  mt-16 md:mt-0'>
-                        <img src='/images/my_pic.jpg'></img>
-                        <div className="flex flex-col pt-5"> 
-                            <p className="text-xl text-gray-500 mt-1">20 September 2021</p>
-                            <h3 className="text-2xl text-gray-900 mt-5">New ballet in our theatre</h3>
-                            <a href='/'>
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 flex-no-shrink fill-current mt-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1} d="M17 8l4 4m0 0l-4 4m4-4H3" />
-                            </svg>
-                            </a>   
-                        </div>
-                    </div>
-                </div>
-                <div className='border-b border-gray-600 mt-16'></div>
+                    
+             </section>
+            <div className='border-b border-gray-600 mt-16'></div>
 
             </div>
             <Footer/>
          </div>
     )
 }
+
