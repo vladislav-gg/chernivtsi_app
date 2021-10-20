d968677648f2a2dcaa5913883047e17178e5cf43
Vladislav <selies@mail.ru>
2021-10-20 01:52:58 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
 import 'tailwindcss/tailwind.css';
-import { Provider } from "next-auth/client";
+import { SessionProvider } from "next-auth/react"
 
-function MyApp({ Component, pageProps }) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
   return (
-  <Provider session={pageProps.session}>
-    <Component {...pageProps} />
-  </Provider>
+    // `session` comes from `getServerSideProps` or `getInitialProps`.
+    // Avoids flickering/session loading on first load.
+    <SessionProvider session={session} refetchInterval={5 * 60}>
+      <Component {...pageProps} />
+    </SessionProvider>
   )
 }
-
-export default MyApp
