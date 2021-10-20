cffa9a3a012ffd30431f5cc3c8efac9c8450be57
Vladislav <selies@mail.ru>
2021-10-20 04:41:44 +0100

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,14 +5,14 @@ export default NextAuth ({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET
-      // authorizationUrl: 
-      //   'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code',
-      // scope:  
-      //   'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/youtube.readonly',
+      clientSecret: process.env.GOOGLE_SECRET,
+      authorizationUrl: 
+        'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code',
+      scope:  
+        'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/youtube.readonly',
 
     }),
   ],
-  debug: false
+  debug: true
 }
 )
