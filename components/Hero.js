f1006cfb9211b6ae10ae3678f9e32279f19bdc45
Vladislav <selies@mail.ru>
2022-01-28 01:33:45 +0000

    changed keen slider for swiper.js. keen caused some weird scroll bug that was imporssible to fix.

--- components/Hero.js
+++ components/Hero.js
@@ -1,18 +1,16 @@
 import React from 'react'
-import { useKeenSlider } from "keen-slider/react"
-import "keen-slider/keen-slider.min.css"
 import Image from 'next/image';
 import PreviewPage from '../pages/checkout';
 import HeroButton from './HeroButton';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 
 
 export default function Hero() {
-	const [sliderRef] = useKeenSlider({ loop: true })
-
 
     return (
-			<div ref={sliderRef} className="keen-slider">
-				<div className="keen-slider__slide relative w-full h-[55rem] ">
+			<Swiper className="mySwiper" loop={true}>
+				<SwiperSlide className="relative w-full h-[55rem] ">
 					<div className="w-full h-full bg-white absolute z-0">
 						<div className='w-full h-[55rem] overflow-hidden relative '>
 							<Image
@@ -24,7 +22,6 @@ export default function Hero() {
 							layout='fill'
 						/>
 						</div>
-						
 					</div>
 					<div className='w-full h-[55rem] bg-gradient-to-t from-black to-transparent opacity-60'></div>
 						<div className='absolute left-10 md:left-44 bottom-40 text-white opacity-90'>
@@ -48,8 +45,8 @@ export default function Hero() {
 							</a>
 							</div>
 						</div>
-				</div>
-				<div className="keen-slider__slide relative w-full h-[55rem]">
+				</SwiperSlide>
+				<SwiperSlide className="relative w-full h-[55rem]">
 					<div className="w-full h-full bg-white absolute z-0">
 						<Image
 							className=""
@@ -61,28 +58,7 @@ export default function Hero() {
 
 						/>
 					</div>
-					{/* WHITE DIV */}
-					{/* <div className="hidden w-[20rem] h-[22rem]  ml-16 mt-12 md:w-[32rem] md:h-[20rem] bg-white absolute z-10 md:mt-40 md:ml-64 ">
-						<h3 className="font-serif font-semibold text-5xl py-5 px-7">
-							Swan Lake
-						</h3>
-						<h4 className="text-lg font-sans text-gray-400 mb-2 px-7 italic">
-							1 March–28 May 2022
-						</h4>
-						<h4 className="text-lg font-sans px-7">
-							The performance lasts about 3 hours, including two intervals.
-						</h4>
-						<h4 className="text-lg font-sans px-7 mt-1">
-							Tickets price: £40-£120
-						</h4>
-						<button
-							type="button"
-							className="text-gray-900  px-10 py-2 border-2 border-gray-600 text-gray-900 hover:bg-gray-900 hover:text-gray-100 duration-700 mt-7 px-7 ml-7 text-lg font-sans"
-						>
-							Buy tickets
-						</button>
-					</div> */}
-					{/* WHITE DIV ENDS */}
+					
 					<div className='w-full h-[55rem] bg-gradient-to-t from-black to-transparent opacity-60'></div>
 					<div className='absolute left-10 md:left-44 bottom-40 text-white opacity-90'>
 						<div>
@@ -106,8 +82,8 @@ export default function Hero() {
 							</a>
 						</div>
 					</div>
-				</div>
-				<div className="keen-slider__slide relative w-full h-[55rem]">
+				</SwiperSlide>
+				<SwiperSlide className="relative w-full h-[55rem]">
 					<div className="w-full h-full bg-white absolute z-0">
 						<Image
 							className=""
@@ -118,26 +94,6 @@ export default function Hero() {
 							objectFit='cover'
 						/>
 					</div>
-					{/* <div className="w-[20rem] h-[22rem] ml-16 mt-12 md:w-[32rem] md:h-[20rem] bg-white absolute z-10 md:mt-40 md:ml-64 ">
-						<h3 className="font-serif font-semibold text-5xl py-5 px-7">
-							The Nutcracker
-						</h3>
-						<h4 className="text-lg font-sans text-gray-400 mb-2 px-7 italic">
-							1 March–28 May 2022
-						</h4>
-						<h4 className="text-lg font-sans px-7">
-							The performance lasts about 3 hours, including two intervals.
-						</h4>
-						<h4 className="text-lg font-sans px-7 mt-1">
-							Tickets price: £40-£120
-						</h4>
-						<button
-							type="button"
-							className="text-gray-900  px-10 py-2 border-2 border-gray-600 text-gray-900 hover:bg-gray-900 hover:text-gray-100 duration-700 mt-7 px-7 ml-7 text-lg font-sans"
-						>
-							Buy tickets
-						</button>
-					</div> */}
 					<div className='w-full h-[55rem] bg-gradient-to-t from-black to-transparent opacity-60'></div>
 						<div className='absolute left-10 md:left-44 bottom-40 text-white opacity-90'>
 							<div>
@@ -161,8 +117,7 @@ export default function Hero() {
 							</a>
 							</div>
 						</div>
-				</div>
-				
-			</div>
+				</SwiperSlide>
+			</Swiper>
 		);
 }
