6ab6b97f93bfdfed234f794230cb3efbef4fb2fe
Vladislav <selies@mail.ru>
2022-02-26 23:11:17 +0000

    recoil bolerplate

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,22 @@
-import 'tailwindcss/tailwind.css';
-import { SessionProvider } from "next-auth/react"
+import "tailwindcss/tailwind.css";
+import { SessionProvider } from "next-auth/react";
+import {
+	RecoilRoot,
+	atom,
+	selector,
+	useRecoilState,
+	useRecoilValue,
+} from "recoil";
 
 export default function App({
-  Component,
-  pageProps: { session, ...pageProps },
+	Component,
+	pageProps: { session, ...pageProps },
 }) {
-  return (
-    // `session` comes from `getServerSideProps` or `getInitialProps`.
-    // Avoids flickering/session loading on first load.
-    <SessionProvider session={session} refetchInterval={5 * 60}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  )
+	return (
+		<RecoilRoot>
+			<SessionProvider session={session} refetchInterval={5 * 60}>
+				<Component {...pageProps} />
+			</SessionProvider>
+		</RecoilRoot>
+	);
 }
