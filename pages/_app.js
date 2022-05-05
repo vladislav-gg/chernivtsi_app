05e08bbe06dc34403c5022ce32fcae43bb2b946c
Vladislav <selies@mail.ru>
2022-05-05 01:17:07 +0100

    changed the image format and amended app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,5 @@
 import "tailwindcss/tailwind.css";
 import { SessionProvider } from "next-auth/react";
-import {
-	RecoilRoot,
-	atom,
-	selector,
-	useRecoilState,
-	useRecoilValue,
-} from "recoil";
 
 export default function App({
 	Component,
