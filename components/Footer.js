d95c8915e97f91967a672d4dbdc15c2d1a329188
Vladislav <selies@mail.ru>
2022-05-19 00:12:27 +0100

    added language for the page in next.config.js

--- components/Footer.js
+++ components/Footer.js
@@ -47,7 +47,6 @@ export default function Footer() {
 								{message}
 							</h2>
 							<div className="mt-5 border-none mb-5 flex justify-center">
-								<label>email</label>
 								<input
 									className="text-black placeholder-gray-900 font-semibold py-3 px-2 w-80 border-b  border-gray-500 md:ml-3 focus:outline-none bg-transparent placeholder-opacity-50"
 									placeholder="Email"
