adc2c7ade1ec26f464631589fa4e5f16e486e449
Vladislav <selies@mail.ru>
2022-06-18 23:47:56 +0100

    added cart

--- components/Navigation.js
+++ components/Navigation.js
@@ -64,6 +64,20 @@ export default function Navigation() {
 					>
 						News
 					</a>
+					{/* <svg
+						xmlns="http://www.w3.org/2000/svg"
+						className="h-7 w-7"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke="currentColor"
+						strokeWidth={2}
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+						/>
+					</svg> */}
 					{loading && (
 						<div className="text-gray-900 font-normal text-lg">Loading...</div>
 					)}
@@ -91,6 +105,7 @@ export default function Navigation() {
 						</button>
 					)}
 				</div>
+				{/* MOBILE NAVIGATION */}
 				<div
 					className={`${
 						active ? "md:hidden" : "hidden"
