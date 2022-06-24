362ac9ec16007f7cb43ab74882524b2bc3121478
Vladislav <selies@mail.ru>
2022-06-24 22:50:18 +0100

    added cart which is connected to Redux global state

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { useSession, signIn, signOut, getSession } from "next-auth/react";
 import { useState } from "react";
-
+import { useSelector, useDispatch } from "react-redux";
+import { selectCount } from "../src/features/counter/counterSlice";
 export default function Navigation() {
 	const { data: session, status } = useSession();
 	const loading = status === "loading";
 	const [active, setActive] = useState(false);
+	const count = useSelector(selectCount);
 
 	// Burger menu toggle
 	const handleClick = () => {
@@ -66,7 +68,7 @@ export default function Navigation() {
 					</a>
 					{/* <svg
 						xmlns="http://www.w3.org/2000/svg"
-						className="h-7 w-7"
+						className="h-7 w-7 after:content-['asdas']"
 						fill="none"
 						viewBox="0 0 24 24"
 						stroke="currentColor"
@@ -101,6 +103,27 @@ export default function Navigation() {
 							Sign Out
 						</button>
 					)}
+					<button
+						class="py-4 px-1 relative border-2 border-transparent text-gray-800 rounded-full hover:text-gray-400 focus:outline-none focus:text-gray-500 transition duration-150 ease-in-out"
+						aria-label="Cart"
+					>
+						<svg
+							class="h-6 w-6"
+							fill="none"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+						>
+							<path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
+						</svg>
+						<span class="absolute inset-0 object-right-top -mr-6">
+							<div class="inline-flex items-center px-1.5 py-0.5 border-2 border-white rounded-full text-xs font-semibold leading-4 bg-gray-500 text-white">
+								{count.quantity}
+							</div>
+						</span>
+					</button>
 				</div>
 				{/* MOBILE NAVIGATION */}
 				<div
