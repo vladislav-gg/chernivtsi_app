7bfba1b5c7fb178ee2bee637ec2e19533aa66196
Vladislav <selies@mail.ru>
2022-06-25 22:53:01 +0100

    cleaned components from junk code

--- components/Button.js
+++ components/Button.js
@@ -3,15 +3,6 @@ import PreviewPage from "../pages/checkout";
 
 export default function Button() {
 	return (
-		// <form action="/api/checkout_sessions" method="POST">
-		//     <button
-		//         type="submit"
-		//         className="text-gray-900  px-10 py-2 border-2 border-gray-600 text-gray-900 hover:bg-gray-900 hover:text-gray-100 duration-700 text-lg font-sans"
-		//     >
-		//         Buy tickets
-		//     </button>
-		// </form>
-
 		<form action="/shows/nutckracker" method="POST">
 			<button
 				type="submit"
--- components/Navigation.js
+++ components/Navigation.js
@@ -3,6 +3,7 @@ import { useSession, signIn, signOut, getSession } from "next-auth/react";
 import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { selectCount } from "../src/features/counter/counterSlice";
+
 export default function Navigation() {
 	const { data: session, status } = useSession();
 	const loading = status === "loading";
@@ -66,20 +67,6 @@ export default function Navigation() {
 					>
 						News
 					</a>
-					{/* <svg
-						xmlns="http://www.w3.org/2000/svg"
-						className="h-7 w-7 after:content-['asdas']"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke="currentColor"
-						strokeWidth={2}
-					>
-						<path
-							strokeLinecap="round"
-							strokeLinejoin="round"
-							d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-						/>
-					</svg> */}
 
 					{session && (
 						<div className="text-gray-900 font-normal text-lg">
