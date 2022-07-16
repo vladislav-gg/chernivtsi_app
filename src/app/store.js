4260850253f2541f4986f9aa74353ff2d83136e6
Vladislav <selies@mail.ru>
2022-07-16 23:09:52 +0100

    added redux-persist to save state on refresh

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,21 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+import thunk from "redux-thunk";
+
+const persistConfig = {
+	key: "root",
+	storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, counterReducer);
 
 export const store = configureStore({
 	reducer: {
-		counter: counterReducer,
+		counter: persistedReducer,
+		middleware: [thunk],
 	},
 });
+
+export const persistor = persistStore(store);
