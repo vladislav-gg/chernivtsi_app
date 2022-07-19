2663f8e3f1b034eb23f289dcf8490c27b689477f
Vladislav <selies@mail.ru>
2022-07-19 21:28:43 +0100

    fixed redux persist type bug

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
 import storage from "redux-persist/lib/storage";
-import { persistReducer, persistStore } from "redux-persist";
+import {
+	persistReducer,
+	persistStore,
+	FLUSH,
+	REHYDRATE,
+	PAUSE,
+	PERSIST,
+	PURGE,
+	REGISTER,
+} from "redux-persist";
 import thunk from "redux-thunk";
 
 const persistConfig = {
@@ -16,6 +25,12 @@ export const store = configureStore({
 		counter: persistedReducer,
 		middleware: [thunk],
 	},
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+			},
+		}),
 });
 
 export const persistor = persistStore(store);
