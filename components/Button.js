36745aca3534bad6b7ca1f45ca9cfd89391ad2be
Vladislav <selies@mail.ru>
2022-07-22 23:17:02 +0100

    changed button styles

--- components/Button.js
+++ components/Button.js
@@ -6,9 +6,9 @@ export default function Button() {
 		<form action="/shows/nutckracker" method="POST">
 			<button
 				type="submit"
-				className="text-gray-900  px-10 py-2 border-2 border-gray-600 text-gray-900 hover:bg-gray-900 hover:text-gray-100 duration-700 text-lg font-sans"
+				className="text-gray-900  px-9 py-2 border-2 border-gray-600 text-gray-900 hover:bg-gray-900 hover:text-gray-100 duration-700 text-lg font-sans"
 			>
-				Buy tickets
+				Proceed to checkout
 			</button>
 		</form>
 	);
